3ad579e7a4c59d803e3364f57fddd928e7ae67d1
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-08-30 07:35:47 -0500

    lo que quieras

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('<html><head></head><body><h1>DONE FUCKED UP</h1></body></html>')
+    res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
 });
 
 const PORT = process.env.PORT || 3000;
