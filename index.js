d78e9be2d5b7893cabfef41edccdeb07dd351acf
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-08-30 11:19:06 -0500

    se agrego lo necesario para trabajar con oauth mediante passport

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const passport = require('passport');
 const express = require('express');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
-});
+// app.get('/', function (req, res) {
+//     res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
+// });
 
-const PORT = process.env.PORT || 3000;
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
