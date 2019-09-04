26f3a2dff12f731fa30671f937af55026cff6541
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-09-04 08:19:50 -0500

    se agregaron los cambios necesarios para trabajar con google auth

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ const express = require('express');
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
 
+const keys = require('./config/keys');
 // app.get('/', function (req, res) {
 //     res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
 // });
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    }));
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
 
