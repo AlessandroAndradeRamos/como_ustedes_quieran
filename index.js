9d89eae2d41447a08556a486ff14082b1187a7f6
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-09-10 07:20:00 -0500

    Se aprendieron mas cosas

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-const passport = require('passport');
 const express = require('express');
-const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express();
+require('./routes/authRoutes') (app);
+const passportConfig = require('./services/passport');
 
-const keys = require('./config/keys');
 // app.get('/', function (req, res) {
 //     res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
 // });
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    }));
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
