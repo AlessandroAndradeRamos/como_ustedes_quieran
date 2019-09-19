da515221a97c71e42dfe7ffc425f8b3f345cb1cb
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-09-18 22:19:32 -0500

    Se modificaron demasiadas cosas

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
-const app = express();
-require('./routes/authRoutes') (app);
-const passportConfig = require('./services/passport');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys')
+require ('./models/users');
+require('./services/passport');
 
-// app.get('/', function (req, res) {
-//     res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
-// });
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+    );
+    
+    app.use(passport.initialize());
+    app.use(passport.session())
+    // app.get('/', function (req, res) {
+        //     res.send('<html><head></head><body><h1>ALGO NOMAS</h1></body></html>')
+        // });
 const PORT = process.env.PORT || 5000;
+require('./routes/authRoutes') (app);
 
 app.listen(PORT);
